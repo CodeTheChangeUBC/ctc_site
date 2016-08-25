88ad92c9b48d775cf6b1b4b0aa5b684375ece63e
grellyd <graham.lloyd.brown@gmail.com>
2016-08-25 02:59:56 +0000

    Migrated to PostgreSQL for Heroku compatability

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160810191948) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "members", force: :cascade do |t|
     t.string   "firstName"
     t.string   "lastName"
