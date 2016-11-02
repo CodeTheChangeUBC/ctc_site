15a3bfb76ad26e2affb1b45147574a855398156f
grellyd <graham.brown137@gmail.com>
2016-11-02 15:50:44 +0000

    fixed db migration typo

--- db/migrate/20161009175615_add_description_to_projects.rb
+++ db/migrate/20161009175615_add_description_to_projects.rb
@@ -1,4 +1,4 @@
-riclass AddDescriptionToProjects < ActiveRecord::Migration[5.0]
+class AddDescriptionToProjects < ActiveRecord::Migration[5.0]
   def change
     add_column :projects, :description, :text
   end
