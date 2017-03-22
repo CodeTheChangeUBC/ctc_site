c9e7c540484b33aaa50dc6538fe5629eb26ab6a5
benchugg <ben.ih.chugg@gmail.com>
2017-03-21 21:47:25 -0700

    change team photo on main page

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,8 @@
 # Omniauth Authentication for Github
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "read:org"
-	# if Rails.env.development?
-	# 	provider :github, ENV['GITHUB_KEY_DEV'], ENV['GITHUB_SECRET_DEV'], scope: "read:org"
- #  	else 
- #  		provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "read:org"
- #  	end
+	if Rails.env.development?
+		provider :github, ENV['GITHUB_KEY_DEV'], ENV['GITHUB_SECRET_DEV'], scope: "read:org"
+  	else 
+  		provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "read:org"
+  	end
 end
