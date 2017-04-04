5b02a903f629661e6c5f023de29fc657aee83b40
grellyd <graham.brown137@gmail.com>
2017-04-04 19:19:52 +0000

    Covered back to tabs as it was before. ewww....

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,11 +4,11 @@ class Member < ActiveRecord::Base
 	mount_uploader :avatar, AvatarUploader
     validates :firstName,  presence: true, length: { maximum: 50 }
     validates :lastName, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, length: { maximum: 255 },
-        format: { with: VALID_EMAIL_REGEX },
-        uniqueness: { case_sensitive: false },
-        allow_nil: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, length: { maximum: 255 },
+		format: { with: VALID_EMAIL_REGEX },
+		uniqueness: { case_sensitive: false },
+		allow_nil: true
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
     validates :subscriber_id, presence: true
