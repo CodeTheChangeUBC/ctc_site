4544619ad7c341946a202cc0a58f3d4971f7a723
grellyd <graham.brown137@gmail.com>
2017-04-04 19:13:36 +0000

    Fixed whitespace

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,12 +2,12 @@ class Member < ActiveRecord::Base
 	attr_accessor :remember_token
 	before_save :downcase_email
 	mount_uploader :avatar, AvatarUploader
-	validates :firstName,  presence: true, length: { maximum: 50 }
-	validates :lastName, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i		
-    validates :email, length: { maximum: 255 },		
-        format: { with: VALID_EMAIL_REGEX },		
-        uniqueness: { case_sensitive: false }, 		
+    validates :firstName,  presence: true, length: { maximum: 50 }
+    validates :lastName, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, length: { maximum: 255 },
+        format: { with: VALID_EMAIL_REGEX },
+        uniqueness: { case_sensitive: false },
         allow_nil: true
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
