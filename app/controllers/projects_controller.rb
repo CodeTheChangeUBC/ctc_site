09bc704ff42899a7aa2bcf5351b9657e8ba02e58
bchugg <ben.ih.chugg@gmail.com>
2017-10-07 19:38:21 -0700

    arrange project index by year

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,7 +20,12 @@ class ProjectsController < ApplicationController
     end
 
     def index
-        @projects = Project.all.order(:created_at)
+        @first_year = 2016; # first year club was active
+        #@projects = Project.all.order(:created_at)
+        @projects_by_year = Array.new(Time.current.year-@first_year+1)
+        (2016..Time.current.year).each do |year|
+            @projects_by_year[year-2016] = Project.all.where(year: year).order(:created_at)
+        end
     end
 
     def edit
@@ -49,7 +54,7 @@ class ProjectsController < ApplicationController
             params.require(:project).permit(:title, :startDate, :endDate, 
                                             :volCap, :volNum, :githubPage, 
                                             :description, :status, :project_url, 
-                                            :avatar)
+                                            :avatar, :year)
         end
 
 end
